fdff9549f1177eaf3a00982bc4a311fd000753a1
Daniel Stelea <daniel@digitnord.no>
2023-12-18 23:05:03 +0200

    day 2 part 1

---
+++ day2/src/main.rs
@@ -0,0 +1,51 @@
+use std::fs;
+
+static MAX_RED: i32 = 12;
+static MAX_GREEN: i32 = 13;
+static MAX_BLUE: i32 = 14;
+
+fn main() {
+    let games: Vec<String> = fs::read_to_string("src/input.txt")
+        .expect("Could not find file input.txt")
+        .lines()
+        .map(|raw| String::from(raw))
+        .collect();
+
+    let valid_count: i32 = games
+        .iter()
+        .map(|game| {
+            let parsed_game:Vec<&str> = game.split(": ").collect();
+            let game_no = parsed_game.first().unwrap().replace("Game ", "").parse::<i32>().expect("Invalid game number");
+
+            let moves = parsed_game
+                .last()
+                .expect(format!("Unable to find a set of moves for game {:?}", game).as_str());
+            let sets: Vec<&str> = moves.split("; ").collect();
+
+            let is_invalid = sets.iter().any(|set| {
+                set.split(", ").any(|raw| {
+                    let val = raw.to_string();
+                    let dat: Vec<&str> = val.split(" ").collect();
+                    let num = dat
+                        .first()
+                        .expect("Invalid number")
+                        .parse::<i32>()
+                        .expect("Invalid number");
+                    let color = dat.last().unwrap().to_owned();
+                    match color {
+                        "red" => num > MAX_RED,
+                        "green" => num > MAX_GREEN,
+                        "blue" => num > MAX_BLUE,
+                        _ => panic!("Invalid color"),
+                    }
+                })
+            });
+
+            if is_invalid {
+                return 0;
+            }
+            return game_no;
+        })
+        .sum();
+    print!("Result {}", valid_count);
+}
