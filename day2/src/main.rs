d326e7c4cbaf5cb47735eb661ef88fdcd443017c
Daniel Stelea <daniel@digitnord.no>
2023-12-18 23:21:00 +0200

    Day 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -4,7 +4,7 @@ static MAX_RED: i32 = 12;
 static MAX_GREEN: i32 = 13;
 static MAX_BLUE: i32 = 14;
 
-fn first_part() {
+fn part_one() {
     let games: Vec<String> = fs::read_to_string("src/input.txt")
         .expect("Could not find file input.txt")
         .lines()
@@ -52,9 +52,57 @@ fn first_part() {
             return game_no;
         })
         .sum();
-    print!("Result {}", valid_count);
+    println!("Part one result {}", valid_count);
+}
+fn part_two() {
+    let games: Vec<String> = fs::read_to_string("src/input.txt")
+        .expect("Could not find file input.txt")
+        .lines()
+        .map(|raw| String::from(raw))
+        .collect();
+
+    let valid_count: i32 = games
+        .iter()
+        .map(|game| {
+            let parsed_game: Vec<&str> = game.split(": ").collect();
+
+            let moves = parsed_game
+                .last()
+                .expect(format!("Unable to find a set of moves for game {:?}", game).as_str());
+            let sets: Vec<&str> = moves.split("; ").collect();
+
+            let mut max_red = 0;
+            let mut max_green = 0;
+            let mut max_blue = 0;
+
+            for set in sets {
+                set.split(", ").for_each(|raw| {
+                    let val = raw.to_string();
+                    let dat = val.split(" ").collect::<Vec<&str>>();
+                    let num = dat
+                        .first()
+                        .expect("Invalid number")
+                        .parse::<i32>()
+                        .expect("Invalid number");
+                    let color = dat.last().unwrap().to_owned();
+                    if color == "red" && num > max_red {
+                        max_red = num;
+                    }
+                    if color == "green" && num > max_green {
+                        max_green = num;
+                    }
+                    if color == "blue" && num > max_blue {
+                        max_blue = num;
+                    }
+                });
+            }
+            max_red * max_green * max_blue
+        })
+        .sum();
+    println!("Part two result {}", valid_count);
 }
 
 fn main() {
-    first_part();
+    part_one();
+    part_two();
 }
